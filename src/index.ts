97c4361e1238b7f8e5343b07385d0edcbc6a5e05
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2021-01-12 22:42:51 -0600

    ft[core/api] => Finish shuffle() func call

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,18 @@ import axios from "axios";
 /**
  * A highly complex function that adds two numbers together.
  */
-export function add() {
-  async function getUser() {
+class CardDeckAPI {
+  async shuffle() {
     try {
       const response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
       console.log(response.data);
+      return response.data;
     } catch (error) {
       console.error(error);
     }
   }
-
-  getUser();
 }
 
-add();
+export const api = new CardDeckAPI();
+
+api.shuffle();
