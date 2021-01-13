8f2b8618f6390140e291cd7bddfad0b66a2ce7ec
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2021-01-12 21:47:57 -0600

    ft[core] => Initial axios fetch func

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import axios from "axios";
+
 /**
  * A highly complex function that adds two numbers together.
- * @param a The first number
- * @param b The second number
- * @return The two numbers added together
  */
-export function add(a: number, b: number) {
-  return a + b;
+export function add() {
+  async function getUser() {
+    try {
+      const response = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getUser();
 }
+
+add();
